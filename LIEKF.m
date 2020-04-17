de450624448ece3a9a7a03f63ac77253356970fc
sboylan <sboylan@umich.edu>
2020-04-17 15:42:55 -0400

    Added Asynchronous measurements

--- LIEKF.m
+++ LIEKF.m
@@ -4,9 +4,9 @@ classdef LIEKF < handle
         mu;         %Pose Mean
         bias;       %Bias of gyro and accelerometer = [wb,ab]'; 
         Sigma;      %Pose Sigma
-        mu_pred;    %Mean after prediction step
-        bias_pred;  %Bias after prediction step
-        Sigma_pred; %Sigma after prediction step
+%         mu_pred;    %Mean after prediction step
+%         bias_pred;  %Bias after prediction step
+%         Sigma_pred; %Sigma after prediction step
         A;          %Process model - Not currently being used
         cov_g;      %gyro noise
         cov_a;      %acc noise
@@ -31,18 +31,19 @@ classdef LIEKF < handle
             obj.cov_gb = eye(3);
             obj.cov_ab = eye(3);
             obj.V = [4.6778    1.9437    1.3148 % Covariance of observation noise (see getZurichData.m)
-                1.9437   11.5621    6.9711
-                1.3148    6.9711   43.9883]; 
+                    1.9437   11.5621    6.9711
+                    1.3148    6.9711   43.9883]; 
             obj.Q = blkdiag([obj.cov_g,zeros(3),zeros(3),zeros(3),zeros(3);
                              zeros(3),obj.cov_a,zeros(3),zeros(3),zeros(3);
                              zeros(3),zeros(3),eye(3),zeros(3),zeros(3);
                              zeros(3),zeros(3),zeros(3),obj.cov_gb,zeros(3),;
                              zeros(3),zeros(3),zeros(3),zeros(3),obj.cov_ab]);
-            obj.A = @(wt,at) [-obj.wedge(wt), zeros(3),  zeros(3), -eye(3), zeros(3); 
-                              -obj.wedge(at), -obj.wedge(wt), zeros(3), zeros(3), -eye(3);
-                               zeros(3), eye(3), -obj.wedge(wt), zeros(3),zeros(3);
-                               zeros(3), zeros(3), zeros(3), zeros(3), zeros(3);
-                               zeros(3), zeros(3), zeros(3), zeros(3), zeros(3)];
+                         
+            obj.A = @(wt,at) [-obj.wedge(wt),   zeros(3),        zeros(3),      -eye(3),   zeros(3); 
+                              -obj.wedge(at),   -obj.wedge(wt),  zeros(3),      zeros(3), -eye(3);
+                               zeros(3),        eye(3),         -obj.wedge(wt), zeros(3),  zeros(3);
+                               zeros(3),        zeros(3),        zeros(3),      zeros(3),  zeros(3);
+                               zeros(3),        zeros(3),        zeros(3),      zeros(3),  zeros(3)];
         end
         
         function prediction(obj, w, a, dt)  %TBC bias
@@ -64,37 +65,39 @@ classdef LIEKF < handle
             R = obj.mu(1:3,1:3);
             v = obj.mu(1:3,4);
             p = obj.mu(1:3,5);
+            
             Rk = R*gamma0(wt*dt);
             vk = v + R*gamma1(wt*dt)*at*dt + [0;0;9.81]*dt;
             pk = p + v*dt + R*gamma2(wt*dt)*at*(dt^2) + 0.5*[0;0;9.81]*(dt^2);
+            
+            % I think this should be A(w,a) not A(wt,at)
             phi = expm(obj.A(wt,at)*dt); 
             
-            obj.mu_pred = [Rk,vk,pk; 0,0,0,1,0; 0,0,0,0,1];
-            obj.bias_pred = zeros(6,1);
-            obj.Sigma_pred = phi*obj.Sigma*(phi') + phi*obj.Q*(phi')*dt;
+            obj.mu = [Rk,vk,pk; 0,0,0,1,0; 0,0,0,0,1];
+            obj.Sigma = phi*obj.Sigma*(phi') + phi*obj.Q*(phi')*dt;
            
         end
                 
         function correction(obj,GPS)    %GPS 3X1
             Y = [GPS;0;1];
             H = [zeros(3),zeros(3), ones(3), zeros(3), zeros(3)];
-            R = obj.mu_pred(1:3,1:3);
+            R = obj.mu(1:3,1:3);
             N = R' * obj.V * R;
             
-            S = H * obj.Sigma_pred * H' + N;
-            K = obj.Sigma_pred * H' / S;     % Kalman gain
+            S = H * obj.Sigma * H' + N;
+            K = obj.Sigma * H' / S;     % Kalman gain
             K_X = K(1:9,:);
             K_B = K(10:15,:);
             PI = [eye(3), zeros(3,2)];
             
-            nu = eye(5)/obj.mu_pred * Y;        % Innovation
+            nu = eye(5)/obj.mu * Y;        % Innovation
             delta_X = K_X * PI * nu;         
             delta_B = K_B * PI * nu;         
             xi = obj.makeTwist(delta_X);     % basically the obj.wedge operator for our 9-dimensional state space
             
-            obj.mu = obj.mu_pred * expm(xi);
-            obj.bias = obj.bias_pred + delta_B;
-            obj.Sigma = (eye(15)- K * H) * obj.Sigma_pred * (eye(15)- K * H)' + K * N * K';
+            obj.mu = obj.mu * expm(xi);
+            obj.bias = delta_B;
+            obj.Sigma = (eye(15)- K * H) * obj.Sigma * (eye(15)- K * H)' + K * N * K';
         end
         
         function R = wedge(obj, v)
