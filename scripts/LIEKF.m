f719d2c4a930706eddc0517b9ad434ca8b762e39
Glen Haggin <ghaggin@umich.edu>
2020-04-20 09:50:23 -0400

    Fixed wedge function, removed mu/sigma predict variables, added comments to prediction

--- scripts/LIEKF.m
+++ scripts/LIEKF.m
@@ -4,9 +4,6 @@ classdef LIEKF < handle
         mu;         %Pose Mean
         bias;       %Bias of gyro and accelerometer = [wb,ab]'; 
         Sigma;      %Pose Sigma
-        mu_pred;    %Mean after prediction step
-        bias_pred;  %Bias after prediction step
-        Sigma_pred; %Sigma after prediction step
         A;          %Process model - Not currently being used
         cov_g;      %gyro noise
         cov_a;      %acc noise
@@ -38,16 +35,20 @@ classdef LIEKF < handle
             obj.cov_gb = eye(3);
             obj.cov_ab = eye(3);
             obj.V = eye(3);
-            obj.Q = blkdiag([obj.cov_g,zeros(3),zeros(3),zeros(3),zeros(3);
-                             zeros(3),obj.cov_a,zeros(3),zeros(3),zeros(3);
-                             zeros(3),zeros(3),eye(3),zeros(3),zeros(3);
-                             zeros(3),zeros(3),zeros(3),obj.cov_gb,zeros(3),;
-                             zeros(3),zeros(3),zeros(3),zeros(3),obj.cov_ab]);
-            obj.A = @(wt,at) [-obj.wedge(wt), zeros(3),  zeros(3), -eye(3), zeros(3); 
-                              -obj.wedge(at), -obj.wedge(wt), zeros(3), zeros(3), -eye(3);
-                               zeros(3), eye(3), -obj.wedge(wt), zeros(3),zeros(3);
-                               zeros(3), zeros(3), zeros(3), zeros(3), zeros(3);
-                               zeros(3), zeros(3), zeros(3), zeros(3), zeros(3)];
+            obj.Q = blkdiag([
+                obj.cov_g,zeros(3),zeros(3),zeros(3),zeros(3);
+                zeros(3),obj.cov_a,zeros(3),zeros(3),zeros(3);
+                zeros(3),zeros(3),eye(3),zeros(3),zeros(3);
+                zeros(3),zeros(3),zeros(3),obj.cov_gb,zeros(3),;
+                zeros(3),zeros(3),zeros(3),zeros(3),obj.cov_ab
+            ]);
+            obj.A = @(wt,at) [
+                -obj.skew(wt), zeros(3),  zeros(3), -eye(3), zeros(3); 
+                -obj.skew(at), -obj.skew(wt), zeros(3), zeros(3), -eye(3);
+                zeros(3), eye(3), -obj.skew(wt), zeros(3),zeros(3);
+                zeros(3), zeros(3), zeros(3), zeros(3), zeros(3);
+                zeros(3), zeros(3), zeros(3), zeros(3), zeros(3)
+            ];
         end
 
         function [R, p, v] = getState(obj)
@@ -57,96 +58,101 @@ classdef LIEKF < handle
         end
         
         function prediction(obj, w, a, dt)  %TBC bias
+            skew = @(u) obj.skew(u);
+
             % Predicts position from gyro/accelerometer data
-            gamma0 = @(phi) eye(3) + sin(norm(phi,2))/norm(phi,2) * obj.wedge(phi) ...
-                + (1-cos(norm(phi,2)))/(norm(phi,2)^2) * (obj.wedge(phi))^2;
+            gamma0 = @(phi) eye(3) + sin(norm(phi,2))/norm(phi,2) * skew(phi) ...
+                + (1-cos(norm(phi,2)))/(norm(phi,2)^2) * (skew(phi))^2;
 
-            gamma1 = @(phi) eye(3) + (1-cos(norm(phi,2)))/(norm(phi,2)^2) * (obj.wedge(phi)) ...
-                + (norm(phi,2) - sin(norm(phi,2)))/(norm(phi,2)^3) * obj.wedge(phi)^2;
+            gamma1 = @(phi) eye(3) + (1-cos(norm(phi,2)))/(norm(phi,2)^2) * (skew(phi)) ...
+                + (norm(phi,2) - sin(norm(phi,2)))/(norm(phi,2)^3) * skew(phi)^2;
 
-            gamma2 = @(phi) 0.5*eye(3) + (norm(phi,2) - sin(norm(phi,2)))/(norm(phi,2)^3) * obj.wedge(phi) ... 
-                + (norm(phi,2)^2 + 2*cos(norm(phi,2)) - 2)/(2*(norm(phi,2)^4)) * obj.wedge(phi)^2;
+            gamma2 = @(phi) 0.5*eye(3) + (norm(phi,2) - sin(norm(phi,2)))/(norm(phi,2)^3) * skew(phi) ... 
+                + (norm(phi,2)^2 + 2*cos(norm(phi,2)) - 2)/(2*(norm(phi,2)^4)) * skew(phi)^2;
 
-            skew = @(u) [
-                0 -u(3) u(2)
-                u(3) 0 -u(1)
-                -u(2) u(1) 0
-            ];
-
+            % Bias stuff?
             wb = obj.bias(1:3); %TBC
             ab = obj.bias(4:6);
             wt = w - wb;    %true noisy value
             at = a - ab;
             
-            R = obj.mu(1:3,1:3);
-            v = obj.mu(1:3,4);
-            p = obj.mu(1:3,5);
-            Rk = R*expm(skew(w)*dt);
+            % Store state in convenient variables
+            [R, p, v] = obj.getState();
 
-            vk = v + R*gamma1(wt*dt)*at*dt + [0;0;-9.81]*dt;
-            pk = p + v*dt + R*gamma2(wt*dt)*at*(dt^2) + 0.5*[0;0;9.81]*(dt^2);
+            % Integrate the angular rates
+            % This is equivalent to 
+            %   R_k = R*expm(skew(w*dt))
+            % only using the gamma function to 
+            % construct the rotation matrix expm(skew(w*dt))
+            Rk = R*gamma0(w*dt);
+
+            % An accelerometer can't tell the difference between
+            % the pull of a gravitational field in one direction
+            % and an acceleration in the opposite direction.
+            % Let g = [0;0;-9.81], then the measured accel a_m is
+            %   a_m = R^T*(a_e - g)
+            % where a_e is the actual acceleration in the earth frame
+            % and R is the rotation of the body frame with respect to earth.
+            % Solving for a_e (to integrate), we get
+            %   a_e = R*a_m + g
+            %
+            % With the earth frame acceleration, integrate once for velocity
+            % and a second time for position.  Formulas with the gamma function
+            % from slides (I don't know the derivation but they appear to work)
+            g = [0;0;-9.81];
+            vk = v + R*gamma1(wt*dt)*at*dt + g*dt;
+            pk = p + v*dt + R*gamma2(wt*dt)*at*(dt^2) + 0.5*g*(dt^2);
             phi = expm(obj.A(wt,at)*dt); 
             
-            obj.mu_pred = [Rk,vk,pk; 0,0,0,1,0; 0,0,0,0,1];
-            obj.bias_pred = zeros(6,1);
-            obj.Sigma_pred = phi*obj.Sigma*(phi') + phi*obj.Q*(phi')*dt;
-
-            obj.mu = obj.mu_pred;
-            obj.Sigma = obj.Sigma_pred;
-           
+            % Set the mean to the predicted value
+            obj.mu = [
+                Rk, vk, pk; 
+                zeros(2,3), eye(2)
+            ];
+            obj.bias = zeros(6,1);
+            obj.Sigma = phi*obj.Sigma*(phi') + phi*obj.Q*(phi')*dt;
         end
                 
-        function correction(obj,GPS)    %GPS 3X1
-            obj.mu_pred = obj.mu;
-            obj.Sigma_pred = obj.Sigma;
-
+        %GPS 3x1
+        function correction(obj,GPS)    
             Y = [GPS;0;1];
             H = [zeros(3),zeros(3), eye(3), zeros(3), zeros(3)];
-            R = obj.mu_pred(1:3,1:3);
+            [R, ~, ~] = obj.getState();
+            
             N = R' * obj.V * R;
             
-            S = H * obj.Sigma_pred * H' + N;
-            K = obj.Sigma_pred * H' / S;     % Kalman gain
+            S = H * obj.Sigma * H' + N;
+            K = obj.Sigma * H' / S;     % Kalman gain
             K_X = K(1:9,:);
             K_B = K(10:15,:);
             PI = [eye(3), zeros(3,2)];
             
-            nu = eye(5)/obj.mu_pred * Y;        % Innovation
+            nu = eye(5)/obj.mu * Y;        % Innovation
             delta_X = K_X * PI * nu;         
             delta_B = K_B * PI * nu;         
             xi = obj.makeTwist(delta_X);     % basically the obj.wedge operator for our 9-dimensional state space
             
-            obj.mu = obj.mu_pred * expm(xi);
+            obj.mu = obj.mu * expm(xi);
             obj.bias = obj.bias_pred + delta_B;
-            obj.Sigma = (eye(15)- K * H) * obj.Sigma_pred * (eye(15)- K * H)' + K * N * K';
+            obj.Sigma = (eye(15)- K * H) * obj.Sigma * (eye(15)- K * H)' + K * N * K';
         end
         
-        function R = wedge(obj, v)
-            G1 = [...
-                0     0     1;
-                0     0     0;
-                0     0     0];
-            G2 = [...
-                0     0     0;
-                0     0     1;
-                0     0     0];
-            G3 = [...
-                0    -1     0;
-                1     0     0;
-                0     0     0];
-
-            R = G1*v(1) + G2*v(2) + G3*v(3);   
+        function skew = skew(obj, u)
+            skew = [
+                0 -u(3) u(2)
+                u(3) 0 -u(1)
+                -u(2) u(1) 0
+            ];  
         end
         
         function xi = makeTwist(obj, delta)
             % from vector 9x1 to se2(3) 5x5
-            R_lie = obj.wedge(delta(1:3));
+            R_lie = obj.skew(delta(1:3));
             v_lie = [delta(4);delta(5);delta(6)];
             p_lie = [delta(7);delta(8);delta(9)];
             xi = [R_lie, v_lie, p_lie;...
                   zeros(1,3),  0,     0;...
                   zeros(1,3),  0,     0];
         end
-        
     end
 end
