542a550de45fe046a61ba77c0871b47de2e70e4f
Cheng Jiang <chengjia@umich.edu>
2020-04-25 20:52:21 -0400

    Added jacobian symath

--- scripts/EKF.m
+++ scripts/EKF.m
@@ -2,8 +2,11 @@ classdef EKF < handle
     properties
         mu;         % pose = [x;v;theta] - 9*1
         Sigma;
-        cov_g;      %gyro noise
-        cov_a;      %acc noise
+        Q_w_mat;    % gyro noise
+        Q_a_mat;    % acc noise
+        mu_sym;
+        F_sym;
+        W_sym;
         V;          %observation noise of position
     end
     
@@ -18,10 +21,45 @@ classdef EKF < handle
             obj.mu = [p0; v0; theta0];
             obj.Sigma = eye(9);
             
-            [obj.cov_g, obj.cov_a, obj.V] = deal(eye(3));
+            [obj.Q_w_mat, obj.Q_a_mat, obj.V] = deal(eye(3) .* 0.01);
+            
+            syms x [3, 1]
+            syms v [3, 1]
+            syms t [3, 1]
+            syms w [3, 1]
+            syms a [3, 1]
+            syms dt
+
+            rotm = obj.rot_mat(t);
+            
+            obj.mu_sym = [x + rotm * v * dt; v + rotm * a * dt; t + w * dt];
+            obj.F_sym = jacobian(obj.mu_sym, [x; v; t]);
+            obj.W_sym = jacobian(obj.mu_sym, [w; a]);
+            
         end
         
-        function rotm = rot_mat(euler_angle)
+        %------------------------------------------------------------------
+        
+        function prediction(obj, w, a, dt)
+            F_mat = obj.eval_F(obj.mu, w, a, dt);
+            W_mat = obj.eval_W(obj.mu, w, a, dt);
+            
+            obj.Sigma = F_mat * obj.Sigma * F_mat' + ...
+                W_mat * blkdiag(obj.Q_w_mat, obj.Q_a_mat) * W_mat';
+            
+            %Propoagate mean through non-linear dynamics
+            obj.mu = obj.eval_mu(obj.mu, w, a, dt);
+        end
+        
+        %------------------------------------------------------------------
+        
+        function correction(obj, z)
+            % TODO
+        end
+        
+        %------------------------------------------------------------------
+        
+        function rotm = rot_mat(~, euler_angle)
             r = euler_angle(1);
             p = euler_angle(2);
             y = euler_angle(3);
@@ -33,23 +71,36 @@ classdef EKF < handle
             rotm = rz * ry * rx;
         end
         
-        function prediction(obj, w, a, dt)
-          
-            %Propoagate mean through non-linear dynamics
+        %------------------------------------------------------------------
+        
+        function out = eval_mu(obj, mu, w, a, dt) %#ok<INUSD>
+            x1 = mu(1); x2 = mu(2); x3 = mu(3);   %#ok<NASGU>
+            v1 = mu(4); v2 = mu(5); v3 = mu(6);   %#ok<NASGU>
+            t1 = mu(7); t2 = mu(8); t3 = mu(9);   %#ok<NASGU>
+            w1 = w(1); w2 = w(2); w3 = w(3);      %#ok<NASGU>
+            a1 = a(1); a2 = a(2); a3 = a(3);      %#ok<NASGU>
             
-            %a = a + [0; 0; -9.81];
-            x = obj.mu(1:3) + rot_mat(obj.mu(4:6)) * dt + ...
-                1 / 2 * rot_mat(obj.mu(4:6)) * a * dt;
-            v = obj.mu(4:6) + rot_mat(obj.mu(4:6)) * a * dt;
-            theta = obj.mu(7:9) + w;
-            obj.mu = [x; v; theta];
-            
-            %Update covariance with G,V and M(u)
-            obj.Sigma_pred = 0; %TODO
+            out = subs(obj.mu);
         end
         
-        function correction(obj, z)
-            % TODO
+        function out = eval_F(obj, mu, w, a, dt) %#ok<INUSD>
+            x1 = mu(1); x2 = mu(2); x3 = mu(3);  %#ok<NASGU>
+            v1 = mu(4); v2 = mu(5); v3 = mu(6);  %#ok<NASGU>
+            t1 = mu(7); t2 = mu(8); t3 = mu(9);  %#ok<NASGU>
+            w1 = w(1); w2 = w(2); w3 = w(3);     %#ok<NASGU>
+            a1 = a(1); a2 = a(2); a3 = a(3);     %#ok<NASGU>
+            
+            out = subs(obj.F_sym);
+        end
+        
+        function out = eval_W(obj, mu, w, a, dt) %#ok<INUSD>
+            x1 = mu(1); x2 = mu(2); x3 = mu(3);  %#ok<NASGU>
+            v1 = mu(4); v2 = mu(5); v3 = mu(6);  %#ok<NASGU>
+            t1 = mu(7); t2 = mu(8); t3 = mu(9);  %#ok<NASGU>
+            w1 = w(1); w2 = w(2); w3 = w(3);     %#ok<NASGU>
+            a1 = a(1); a2 = a(2); a3 = a(3);     %#ok<NASGU>
+            
+            out = subs(obj.W_sym);
         end
     end
 end
